d365ff8ed7f19062983b3a3f93d0da124cd5847e
Rodrigo Ignacio Falco <rfalco@patagoniansys.com>
2014-07-16 12:47:04 -0300

    Initial project commit.

---
+++ desktop/src/com/monoxidos/battle/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.monoxidos.battle.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.monoxidos.battle.BattlePrototype;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new BattlePrototype(), config);
+	}
+}
